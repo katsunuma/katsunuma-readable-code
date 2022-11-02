227005aba859d2f9045b882e9938a625cf18c3f9
katsunuma <sample@sample>
2022-11-02 12:13:16 +0900

    spec6: 単語出力をIDで指定できるように変更

--- output_word.py
+++ output_word.py
@@ -1,9 +1,18 @@
+import sys
+
 import json
 
 #辞書データの読み込み
 with open('./word_data.json') as f:
     dic_data = json.load(f)
 
-#辞書データに登録されているidと単語をすべて出力
-for word_data in dic_data:
+#引数の数によって挙動を変更する
+if len(sys.argv) == 2:
+    #引数で指定されたidの単語を出力
+    word_data = dic_data[int(sys.argv[1])]
     print(str(word_data["id"])+": " + word_data["word"])
+
+else:
+    #辞書データに登録されているidと単語をすべて出力
+    for word_data in dic_data:
+        print(str(word_data["id"])+": " + word_data["word"])
